37c87feec3e9ebc599795d735b8d9e8755c7bf43
felipeoliveir44 <felipeoliveiragg@outlook.com>
2024-02-22 17:58:47 -0300

    Initial commit

---
+++ cookin-up/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="banner">
+    <div class="apresentacao">
+      <img src="./assets/imagens/logo.svg" alt="Logo do Cookin'up" class="logo">
+
+      <p class="cabecalho-lh frase-cabecalho">
+        <span class="texto-verde">Um banquete de ideias para despertar o chef que há em você!</span>
+      </p>
+
+      <p class="subtitulo-lg">
+        Explore novas receitas todos os dias com os ingredientes que estão ao seu alcance!
+      </p>
+
+      <img src="./assets/imagens/foto-banner.png" alt="" class="foto-banner">
+    </div>
+  </header>
+</template>
