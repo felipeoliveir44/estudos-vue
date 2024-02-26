4fc28e3694bcdb20be2bc4ca87f40aafc1c8cd80
felipeoliveir44 <felipeoliveiragg@outlook.com>
2024-02-26 00:07:54 -0300

    Commit

--- cookin-up/src/App.vue
+++ cookin-up/src/App.vue
@@ -1,17 +1,24 @@
+
+<script lang="ts">
+// Aqui dizemos que iremos utilizar o TypeScript
+// e logo fazemos a importação do componente banner do arquivo banner.vue
+
+import Banner from './components/Banner.vue';
+
+// Para dizermos que o vue realmente precisa utilizar esse componente
+
+export default {
+  // Aqui declaramos os componentes que este componente irá usar
+  // Utilizamos o export, falando que o componente que queremos usar é o banner com o valor de banner
+  components: { Banner }
+  // Uma sintexe é quando o nome do propriedade é igual seu valor podemos deixar apenas o nome da propriedade
+}
+</script>
+
 <template>
-  <header class="banner">
-    <div class="apresentacao">
-      <img src="./assets/imagens/logo.svg" alt="Logo do Cookin'up" class="logo">
-
-      <p class="cabecalho-lh frase-cabecalho">
-        <span class="texto-verde">Um banquete de ideias para despertar o chef que há em você!</span>
-      </p>
-
-      <p class="subtitulo-lg">
-        Explore novas receitas todos os dias com os ingredientes que estão ao seu alcance!
-      </p>
-
-      <img src="./assets/imagens/foto-banner.png" alt="" class="foto-banner">
-    </div>
-  </header>
+  <Banner />
 </template>
+
+<style scoped>
+
+</style>
