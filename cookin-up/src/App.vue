a1fe2335eab62cf78dd51ce5ea17ec5d4218d289
felipeoliveir44 <felipeoliveiragg@outlook.com>
2024-05-09 12:57:41 -0300

    Commit

--- cookin-up/src/App.vue
+++ cookin-up/src/App.vue
@@ -4,19 +4,21 @@
 // e logo fazemos a importação do componente banner do arquivo banner.vue
 
 import Banner from './components/Banner.vue';
+import Main from './components/Main.vue';
 
 // Para dizermos que o vue realmente precisa utilizar esse componente
 
 export default {
   // Aqui declaramos os componentes que este componente irá usar
   // Utilizamos o export, falando que o componente que queremos usar é o banner com o valor de banner
-  components: { Banner }
+  components: { Banner, Main }
   // Uma sintexe é quando o nome do propriedade é igual seu valor podemos deixar apenas o nome da propriedade
 }
 </script>
 
 <template>
   <Banner />
+  <Main></Main>
 </template>
 
 <style scoped>
